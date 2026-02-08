0ba4942ce812af61bcc67982738db997711735cd
Abrelle Yin Lopez <abrellelopez@gmail.com>
2026-02-08 22:53:33 +0800

    added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning style={{ overflowX: 'hidden' }}>
             <head>
+                <link rel="icon" href="/icons/WebLogo.png" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@400;500;600;700&family=Encode+Sans+Condensed:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
